e522ab243335169cb80424b0529552e1840071f4
marybeth00 <marybethsoriano67@gmail.com>
2024-05-25 19:36:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ const btnLike2 = document.getElementById("btnLike2")
 const count2 = document.getElementById("count2")
 
 function countLike2() {
-  let likeCounts = parseInt(count2
-                            .value) + 1
+  let likeCounts = parseInt(count2.value) + 1
   count2.textContent = likeCounts.toString()
 }
 
