6d9e3da04d3631775d8fa8fee2670cc9a025a12e
marybeth00 <marybethsoriano67@gmail.com>
2024-06-08 10:54:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ btnDislike2.addEventListener("click",hitDislikes2)
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
-function submitComment(){
+
+function submitComment(){ 
 commentbox.textContent += comment.value.toString() + "\n"
 }
 submit.addEventListener("click", submitComment)
