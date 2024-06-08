0cda979bb29b08b107bda00dfd56e7375ab38fd3
marybeth00 <marybethsoriano67@gmail.com>
2024-06-08 10:49:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,5 @@ const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
 function submitComment(){
 commentbox.textContent += comment.value.toString() + "\n"
-
 }
-
 submit.addEventListener("click", submitComment)
