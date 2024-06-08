1a050434810b589efc1c311639be76dd7eec8e15
marybeth00 <marybethsoriano67@gmail.com>
2024-06-08 10:03:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
-const btnDislike1 = document.getElementById("btnDislikes1")
-const btnDislike2 = document.getElementById("btnDislikes2")
+const btnDislike1 = document.getElementById("btnDislike1")
+const btnDislike2 = document.getElementById("btnDislike2")
 
-const countDislikes1 = document.getElementById("countDislikes1")
-const countDislikes2 = document.getElementById("countDislikes2")
+const countDislikes1 = document.getElementById("countDislike1")
+const countDislikes2 = document.getElementById("countDislike2")
 
 function hitLikes1() {
    let totalLikes1 = parseInt(countLikes1.value) + 1
